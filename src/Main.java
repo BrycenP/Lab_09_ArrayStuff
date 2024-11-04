22637711297f5039b6ea64a14f4063d0535933b4
BrycenP <perezb3@mail.uc.edu>
2024-11-03 20:42:18 -0500

    task 2 and 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int[] dataPoints;
+        int randMin = 1;
+        int randMax = 100;
         dataPoints = new int[100];
+        Random rando = new Random();
+
+        for (int x = 0; x <dataPoints.length; x++){
+            dataPoints[x] = rando.nextInt(randMax)+randMin;
+        }
+        for (int y = 0; y < dataPoints.length; y++){
+            System.out.print(dataPoints[y] + " | ");
+        }
 
     }
 }
