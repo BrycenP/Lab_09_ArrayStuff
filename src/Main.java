892195ceb0183f44bdcb552444ba92dae0b358fc
BrycenP <perezb3@mail.uc.edu>
2024-11-03 22:10:25 -0500

    task 9

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,36 @@ public class Main {
         int[] dataPoints;
         int randMin = 1;
         int randMax = 100;
-        int dataSum = 0;
-        int dataMean;
-        int userInput;
-        boolean valIn = false;
+        int maxCount = 0;
+        int minCount = 0;
+        int valMin = 1;
+        int valMax = 1;
         dataPoints = new int[100];
         Random rando = new Random();
         Scanner in = new Scanner(System.in);
-
-        userInput = SafeInput.getRangedInt(in, "enter a value 1-100", 1, 100);
 
         for (int x = 0; x <dataPoints.length; x++){
             dataPoints[x] = rando.nextInt(randMax)+randMin;
         }
         for (int y = 0; y < dataPoints.length; y++){
             System.out.print(dataPoints[y] + " | ");
-            if (dataPoints[y] == userInput){
-                int pos = y+1;
-                System.out.println();
-                System.out.println("SHORT CIRCUIT!!! Your input of " + userInput + " appeared in the position " + pos + " of the array");
-                valIn = true;
-                break;
+        }
+        for (int z = 0; z < dataPoints.length; z++){
+            if (valMin >= dataPoints[z] ) {
+                valMin = dataPoints[z];
+            } else if (valMax <= dataPoints[z]) {
+                valMax = dataPoints[z];
             }
         }
-        if (!valIn){
-            System.out.println();
-            System.out.println("Your value of " + userInput + " did not appear in the array");
+        for (int i = 0; i < dataPoints.length; i++){
+            if (dataPoints[i] == valMin){
+                minCount = minCount + 1;
+            } else if (dataPoints[i] == valMax) {
+                maxCount = maxCount + 1;
+            }
         }
+        System.out.println();
+        System.out.println("The maximum value of the array was " + valMax + " and it appeared " + maxCount + " times");
+        System.out.println("The minimum value of the array was " + valMin + " and it appeared " + minCount + " times");
     }
 }
