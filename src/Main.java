01a2683c33fbca79a7f975ace2a10f20c6304894
BrycenP <perezb3@mail.uc.edu>
2024-11-03 20:51:08 -0500

    task 4

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         int[] dataPoints;
         int randMin = 1;
         int randMax = 100;
+        int dataSum = 0;
+        int dataMean;
         dataPoints = new int[100];
         Random rando = new Random();
 
@@ -11,8 +13,13 @@ public class Main {
             dataPoints[x] = rando.nextInt(randMax)+randMin;
         }
         for (int y = 0; y < dataPoints.length; y++){
+            dataSum = dataPoints[y] + dataSum;
             System.out.print(dataPoints[y] + " | ");
         }
+        dataMean = dataSum / 100;
+        System.out.println();
+        System.out.println("The sum of all the values is " + dataSum);
+        System.out.println("The average of all the values is " + dataMean);
 
     }
 }
